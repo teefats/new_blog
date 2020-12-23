06814c102fad276319da1bdc0a4436b0abed6573
teefats <tee_fats@yahoo.com>
2020-12-23 23:08:44 +0000

    Added templates and pagination

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,12 @@
 from django.db import models
-from django.urls.base import reverse
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
-
-
-# Create your models here.
-
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager, self).get_queryset()\
-            .filter(status='published')
+        return super().get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -19,18 +14,22 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date= 'publish')    
-    author = models.ForeignKey(User, max_length=100, on_delete=models.CASCADE, related_name='blog_posts')
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices = STATUS_CHOICES, default='draft')
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
 
-    objects = models.Manager() #The default Manager
-    published = PublishedManager() #Custom manager
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ('-publish',)
@@ -38,12 +37,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
     def get_absolute_url(self):
-        return reverse('blog:post_detail', 
-                        args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
-
-
-
-    
-
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
-
-from . import views
 from django.urls import path
+from . import views
+
 
 app_name = 'blog'
 
 urlpatterns = [
-    #post views
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:mont>/<int:day>/<slug:post>/',views.post_detail, name='post_detail')
+    # post views
+    path('', views.PostListView.as_view(), name='post_list'),    
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'),
 ]
 
